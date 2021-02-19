923c7306db20800fe35979af6c6ca14affb9dc17
YasserYka <yasseryka@gmail.com>
2021-02-20 02:14:53 +0300

    home controller

--- app/controllers/home.py
+++ app/controllers/home.py
@@ -1,8 +1,24 @@
 # -*- coding: utf-8 -*-
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, url_for
 
 blueprint = Blueprint('home', __name__)
 
-@blueprint.route('/')
+@blueprint.route('/home')
 def index():
-    return render_template('home/index.html')
+    return render_template("home/Home.html")
+
+@blueprint.route('/signuptemplate')
+def signup():
+    return render_template("accounts/Signup.html")
+
+@blueprint.route('/logintemplate')
+def login():
+    return render_template("accounts/Login.html")
+
+@blueprint.route('/usertemplate')
+def user_template():
+    return render_template("auction/users.html")
+
+@blueprint.route('/productformtemplate')
+def product_form():
+    return render_template("auction/ProductForm.html")
