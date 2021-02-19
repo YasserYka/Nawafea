537d2d356e81c03c2abde62447d807236b0a7353
YasserYka <yasseryka@gmail.com>
2021-02-20 02:19:28 +0300

    db config

--- app/extensions.py
+++ app/extensions.py
@@ -1,4 +1,21 @@
-"""Extensions module - Set up for additional libraries can go in here."""
+import os
 from flask_sqlalchemy import SQLAlchemy
 
+database_filename = "database.db"
+project_dir = os.path.dirname(os.path.abspath(__file__))
+database_path = "sqlite:///{}".format(os.path.join(project_dir, database_filename))
+
 db = SQLAlchemy()
+
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = database_path
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+
+    with app.app_context():
+        db_drop_and_create_all()
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
